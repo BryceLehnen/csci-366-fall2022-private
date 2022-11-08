5a4c654edfdda1f570e347bba1efd6185a8bbdf5
BryceLehnen <dlkl46.bl@gmail.com>
2022-11-08 09:50:49 -0700

    SDIV implemented

--- c/lmsm/src/lmsm.c
+++ c/lmsm/src/lmsm.c
@@ -146,6 +146,18 @@ void lmsm_i_smul(lmsm *our_little_machine) {
 }
 
 void lmsm_i_sdiv(lmsm *our_little_machine) {
+    if (!check_stack(our_little_machine)) {
+        return;
+    }
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *next = current->next;
+    lmsm_stack *new = malloc(sizeof(lmsm_stack));
+    new->value = next->value / current->value;
+    new->next = next->next;
+    our_little_machine->accumulator = new;
+
+    free(current);
+    free(next);
 }
 
 void lmsm_i_out(lmsm *our_little_machine) {
@@ -258,6 +270,8 @@ void lmsm_exec_instruction(lmsm *our_little_machine, int instruction) {
         lmsm_i_smin(our_little_machine);
     } else if (927 == instruction) {
         lmsm_i_smul(our_little_machine);
+    } else if (928 == instruction) {
+        lmsm_i_sdiv(our_little_machine);
     } else {
         our_little_machine->error_code = ERROR_UNKNOWN_INSTRUCTION;
         our_little_machine->status = STATUS_HALTED;
